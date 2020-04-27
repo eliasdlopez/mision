a9d0a895e5a622a05d433fb4b90c3642867e267c
Elias <eliasdamianlopez@gmail.com>
2020-04-27 13:05:11 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mision', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.on('error', function(error) {
+    console.error(error);
+});
+
+const UserSchema = new mongoose.Schema({
+    user: {type: String},
+    email: {type: String},
+    password: {type: String}
+});
+
+const Users = mongoose.model('Users', UserSchema);
+
+app.use(express.json());
+
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
+  app.post('/register', async (req,res) => { //registra el usuario en la base de datos
+    const user = req.body.user;
+    const email = req.body.email;
+    const password = bcrypt.hashSync(req.body.password, 6);
+
+    console.log(req.body);
+
+    const userReg = new Users ({ 
+        user: user, 
+        email: email, 
+        password: password 
+    });
+    await userReg.save();
+
+    res.send(console.log('Usuario registrado en la DB'));
+  });
+
+  app.get('/', (req, res) => { // muestra todos los documentos
+      const user = Users.find(function(error, articles) {
+         return res.send(JSON.stringify(articles)); 
+      });
+  });
+
+  app.listen(3000, () => console.log('Listening on port 3000!'));
---
+++ index.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $('.tabla-container').hide();
+    console.log('Se escondió la tabla')
+});
+
+$('form').on('submit', function(event) { //consigue datos de la tabla
+    event.preventDefault();
+    const user = $('#user').val();
+    const email = $('#email').val();
+    const password = $('#password').val();
+    console.log('User', user);
+    console.log('Email', email);
+
+    const tr = (`<tr><td>${user}</td><td>${email}</td></tr>`); // agrega el último elemento de la tabla(no encuentro manera de recargar la tabla correctamente de una)
+
+    $('table').append(tr); //agrega el último elemento a la tabla
+    $.ajax({    //envía la tabla
+        method: 'POST',
+        url: 'http://localhost:3000/register',
+        data: JSON.stringify({
+            user: user,
+            email: email,
+            password: password,
+        }),
+        contentType: 'application/json'
+    }); 
+
+    $('.formulario-container').hide(); //esconde form
+
+
+    $.getJSON('http://localhost:3000/', function (data){ // construye la tabla
+        $.each(data, function(i, item) {
+            $('<tr>').append(
+                $('<td>').html(item.user),
+                $('<td>').html(item.email),
+            ).appendTo('table');
+        });
+    });
+
+    $('.tabla-container').show(); //muestra tabla
+});
