c91496314b0b52e77c46933d5fec10a85fdc5fe4
Goye <karlos.goye@gmail.com>
2020-05-10 22:00:49 -0500

    fix: issue with asynchronicity

--- app.js
+++ app.js
@@ -1,50 +1,60 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mision', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  process.env.MONGODB_URL || "mongodb://localhost:27017/mision",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-mongoose.connection.on('error', function(error) {
-    console.error(error);
+mongoose.connection.on("error", function (error) {
+  console.error(error);
 });
 
 const UserSchema = new mongoose.Schema({
-    user: {type: String},
-    email: {type: String},
-    password: {type: String}
+  user: { type: String },
+  email: { type: String },
+  password: { type: String },
 });
 
-const Users = mongoose.model('Users', UserSchema);
+const Users = mongoose.model("Users", UserSchema);
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-  });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-  app.post('/register', async (req,res) => { //registra el usuario en la base de datos
-    const user = req.body.user;
-    const email = req.body.email;
-    const password = bcrypt.hashSync(req.body.password, 6);
+app.post("/register", async (req, res) => {
+  //registra el usuario en la base de datos
+  const user = req.body.user;
+  const email = req.body.email;
+  const password = bcrypt.hashSync(req.body.password, 6);
 
-    console.log(req.body);
-
-    const userReg = new Users ({ 
-        user: user, 
-        email: email, 
-        password: password 
+  try {
+    const userReg = new Users({
+      user: user,
+      email: email,
+      password: password,
     });
     await userReg.save();
 
-    res.send(console.log('Usuario registrado en la DB'));
-  });
+    res.sendStatus(200);
+  } catch (e) {
+    res.error(e.message);
+  }
+});
 
-  app.get('/', async (req, res) => { // muestra todos los documentos
-      const user = await Users.find();
-         res.send(JSON.stringify(user)); 
-  });
+app.get("/", async (req, res) => {
+  // muestra todos los documentos
+  const user = await Users.find();
+  res.send(JSON.stringify(user));
+});
 
-  app.listen(3000, () => console.log('Listening on port 3000!'));
+app.listen(3000, () => console.log("Listening on port 3000!"));
--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-$(document).ready(function() {
-    $('.tabla-container').hide();
-    console.log('Se escondió la tabla')
+$(document).ready(function () {
+  $(".tabla-container").hide();
 });
 
-$('form').on('submit', function(event) { //consigue datos de la tabla
-    event.preventDefault();
-    const user = $('#user').val();
-    const email = $('#email').val();
-    const password = $('#password').val();
-    console.log('User', user);
-    console.log('Email', email);
+$("form").on("submit", function (event) {
+  //consigue datos de la tabla
+  event.preventDefault();
+  const user = $("#user").val();
+  const email = $("#email").val();
+  const password = $("#password").val();
+  console.log("User", user);
+  console.log("Email", email);
 
-    $.ajax({    //envía la tabla
-        method: 'POST',
-        url: 'http://localhost:3000/register',
-        data: JSON.stringify({
-            user: user,
-            email: email,
-            password: password,
-        }),
-        contentType: 'application/json'
-    }); 
+  $.ajax({
+    //envía la tabla
+    method: "POST",
+    url: "http://localhost:3000/register",
+    data: JSON.stringify({
+      user: user,
+      email: email,
+      password: password,
+    }),
+    contentType: "application/json",
+  }).done(() => {
+    $(".formulario-container").hide(); //esconde form
 
-    $('.formulario-container').hide(); //esconde form
-
-
-    $.getJSON('http://localhost:3000/', function (data){ // construye la tabla
-        $.each(data, function(i, item) {
-            $('<tr>').append(
-                $('<td>').html(item.user),
-                $('<td>').html(item.email),
-            ).appendTo('table');
-        });
+    $.getJSON("http://localhost:3000/", function (data) {
+      // construye la tabla
+      $.each(data, function (i, item) {
+        $("<tr>")
+          .append($("<td>").html(item.user), $("<td>").html(item.email))
+          .appendTo("table");
+      });
     });
 
-    $('.tabla-container').show(); //muestra tabla
+    $(".tabla-container").show(); //muestra tabla
+  });
 });
